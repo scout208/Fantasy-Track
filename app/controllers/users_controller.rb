34338c6b115f64c4f6d009025fdbcd35cfcf9344
kevinlewis2 <kevin-lewis@uiowa.edu>
2015-11-04 18:08:14 +0000

    Added bcrypt gem and some user stuff and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
 
+  #user_params might need some work. Not sure if it is supposed to accept confirm_password
   def user_params
-    params.require(:user).permit(:user_id,:email)  
+    params.require(:user).permit(:user_id,:email, :password, :confirm_password)  
   end
   
   def new
+    
   end
 
   def create
-    
+    @user = User.new(user_params)
+	  if @user.save
+		  flash[:notice] = “signup successful”
+		  redirect_to some_home_page
+	  else
+		  flash[:notice] = "failed save user"
+		  render ‘new’
+	  end
   end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+    has_secure_password #if bcrypt installed
+	before_save {|user| user.email = user.email.downcase }
+	validates :name, presence: true, length: {mximum: 50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true,
+		format: {with: VALID_EMAIL_REGEX},
+		uniqueness: {case_sensitive: false}
+
+	validates :password, presence: true, length: {minimum: 6}
+	validates :password_confirmation, presence: true
 end
