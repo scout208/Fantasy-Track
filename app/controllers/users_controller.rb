3428f542a27a2334403e169f060cde27eff3941e
scout208 <connor-early@uiowa.edu>
2015-11-06 23:05:55 +0000

    Sign up is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:user_id, :email, :password,
                                    :password_confirmation)
     end
 end
