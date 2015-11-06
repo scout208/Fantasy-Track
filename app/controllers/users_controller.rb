94144c7fa7760fe4ab366aadb9f4acbe5fa8d682
scout208 <connor-early@uiowa.edu>
2015-11-06 16:45:48 +0000

    Added a few things to help with sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,28 @@
 class UsersController < ApplicationController
 
-  #user_params might need some work. Not sure if it is supposed to accept confirm_password
-  def user_params
-    params.require(:user).permit(:user_id,:email, :password, :confirm_password)  
-  end
-  
-  def new
-    
-  end
-  
   def show
+    @user = User.find(params[:id])
   end
 
-  def create
-    @user = User.new(user_params)
-	  if @user.save
-		  flash[:notice] = “signup successful”
-		  redirect_to some_home_page
-	  else
-		  flash[:notice] = "failed save user"
-		  render ‘new’
-	  end
+  def new
+    @user = User.new
   end
-    
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_secure_password #if bcrypt installed
 	before_save {|user| user.email = user.email.downcase }
 	before_save :create_session_token
-	validates :name, presence: true, length: {mximum: 50}
+	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true,
 		format: {with: VALID_EMAIL_REGEX},
