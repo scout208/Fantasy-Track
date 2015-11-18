486bc02933a613e9566cf24e3b55d6b172cc3012
yujhuang <yujun-huang@uiowa.edu>
2015-11-18 18:42:10 +0000

    host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {:host => "natasha-yujhuang.c9.io"}
+  config.action_mailer.default_url_options = {:host => "git.heroku.com/secret-gorge-8997.git"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
