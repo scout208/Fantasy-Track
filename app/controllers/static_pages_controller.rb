7020aa5eabe50aaf589d9aee98afb04e4c0ffaec
nate_pierotti <nathan-pierotti@uiowa.edu>
2015-11-18 19:38:19 +0000

    successfully getting all image links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,14 +15,17 @@ class StaticPagesController < ApplicationController
       doc = Nokogiri::HTML(open("http://www.flotrack.org/"))
       @links = doc.css('section#recentBanners a')
       @titles = doc.css('section#recentBanners').css('strong').to_ary
-      @title_images = doc.css('section#recentBanners').attr('src')
+      @title_images = doc.css('section#recentBanners').css('a img')
       
       puts(@titles)
-      @hrefs = @links.map {|link| link.attribute('href').to_s}.uniq.sort.delete_if {|href| href.empty?}
+      @hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
       @hrefs.to_ary
       puts("###########################################################################################")
       
       puts(@hrefs)
+      puts("###########################################################################################")
+
+      puts(@title_images)
 
   end
   
