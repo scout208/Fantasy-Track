c91df50ba2b6989c7c8d49d0b89e8b4c9011bd85
nate_pierotti <nathan-pierotti@uiowa.edu>
2015-11-18 20:49:37 +0000

    images are uploaded!

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,27 +5,20 @@ class StaticPagesController < ApplicationController
   
   def home
     if(logged_in?)
-      #redirect_to @current_user
+    
     end
 
-      #doc = Nokogiri::HTML(open("http://www.flotrack.org/"))
-      #text = doc.css("#topStory")
-      #text = doc.css('li')[0].text
-      #puts(text)
       doc = Nokogiri::HTML(open("http://www.flotrack.org/"))
       @links = doc.css('section#recentBanners a')
       @titles = doc.css('section#recentBanners').css('strong').to_ary
       @title_images = doc.css('section#recentBanners').css('a img')
-      
       puts(@titles)
       @hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
       @hrefs.to_ary
-      puts("###########################################################################################")
-      
       puts(@hrefs)
-      puts("###########################################################################################")
-
-      puts(@title_images)
+      @srcs = @title_images.map {|images| images.attribute('src').to_s}.delete_if {|src| src.empty?}
+      @srcs.to_ary
+      puts(@srcs)
 
   end
   
