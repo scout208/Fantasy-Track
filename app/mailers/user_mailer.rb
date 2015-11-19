b773118d5ea01cde8acbd02a9b9bb6ea4181965b
yujhuang <yujun-huang@uiowa.edu>
2015-11-19 01:11:33 +0000

    config the production enviroment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,5 @@
 class UserMailer < ActionMailer::Base
     default :from => "\"NOREPLY@Fantasy-Track\"<fantasytrack1.0@gmail.com>"
-    default :host => "secret-gorge-8997.herokuapp.com",  :only_path => true
 
     
     def registration_confirmation(user)
