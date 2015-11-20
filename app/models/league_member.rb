4a4f3b613a3e17682777541148cc2c02922ff82d
kevinlewis2 <kevin-lewis@uiowa.edu>
2015-11-20 18:04:43 +0000

    member can't be added to league twice

--- app/models/league_member.rb
+++ app/models/league_member.rb
@@ -1,4 +1,6 @@
 class LeagueMember < ActiveRecord::Base
     belongs_to :league, class_name: "League"
     belongs_to :user, class_name: "User"
+    
+    validates_uniqueness_of :user_id, scope: :league_id
 end
