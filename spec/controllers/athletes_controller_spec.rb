6e87f2804ff79c9333e827eadc09ef744fd107e6
drmopper <xi-chen-1@uiowa.edu>
2015-11-20 21:35:28 +0000

    simplecov for cucumber

--- spec/controllers/athletes_controller_spec.rb
+++ spec/controllers/athletes_controller_spec.rb
@@ -4,10 +4,12 @@ require 'rails_helper'
 describe AthletesController, type: :controller do
     describe 'Create New Athlete' do
         before :each do
-            @a = Athlete.new(:first_name=>"FirstName",:last_name=>"LastName",:birth_date=>"1/1/1990",:country=>"Country",:notes=>"notes")
+            @a = Athlete.create(:first_name=>"FirstName",:last_name=>"LastName",:birth_date=>"1/1/1990",:country=>"Country",:notes=>"notes")
+        end
+        it 'should create a new athelete' do
+            @a = Athlete.create(:first_name=>"FirstName",:last_name=>"LastName",:birth_date=>"1/1/1990",:country=>"Country",:notes=>"notes")
+            expect(@a).to be_an_instance_of Athlete
         end
     end
-    
-    
     
 end
