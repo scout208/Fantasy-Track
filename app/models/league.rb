09d32d505567cdc51751a83ccadf7443cab787ab
nate_pierotti <nathan-pierotti@uiowa.edu>
2015-11-20 01:38:14 +0000

    feature not yet complete

--- app/models/league.rb
+++ app/models/league.rb
@@ -10,11 +10,11 @@ class League < ActiveRecord::Base
 											
 	
 	
-  def self.search(search)
-    if search
+ def self.search(search)
+   if search
      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-    else
-      find(:all)
-    end
-  end
+   else
+     find(:all)
+   end
+ end
 end
