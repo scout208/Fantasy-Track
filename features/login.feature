daa1c853eabae0968eff2f32ee1a4afce63329af
yujhuang <yujun-huang@uiowa.edu>
2015-11-20 11:32:40 +0000

    finished most of the user login cucumber

--- features/login.feature
+++ features/login.feature
@@ -1,18 +1,37 @@
 Feature: Allow user to log in
+  
+  As a user I want to go to login screen quickly from home screen
+  
+Background: the users have been added to user table
+  
+  Given the following users have been added to FantasyTrack:
+  |  user_id   |     email          | role   |  password   | 
+  |   Admin    | admin@gmail.com    | admin  |   password  |
+  |   tester   | tester@testing.com |  user  |    tester   |
+  
+  And I am on the Fantasy-Track home page
  
 Scenario: User finds login page
-  Given I am on the Fantasy-Track home page 
   And I press the login button
   Then I should see the view where I am able to login
+
  
 Scenario: Admin User logs in
   When I am on the login screen
   And I added an email of "admin@gmail.com" and a password "password"
-  And I press "login"
+  And I press "Log in"
+  Then I should be redirected to homepage
   Then I should be signed in as the administrator
   
 Scenario: Regular User logs in
   When I am on the login screen
-  And I added an email of "nathanpierotti@gmail.com" and a password "npierotti"
-  And I press "login"
+  And I added an email of "tester@testing.com" and a password "tester"
+  And I press "Log in"
+  Then I should be redirected to homepage
   Then I should be signed in as a user who is not the administrator
+  
+Scenario: fake User logs in
+  When I am on the login screen
+  And I added an email of "fake@testing.com" and a password "fake"
+  And I press "Log in"
+  Then I should see "Invalid email/password combination"
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,31 +1,60 @@
 Given (/^I am on the Fantasy\-Track home page$/) do
-  visit root # express the regexp above with the code you wish you had
+  visit root_url # express the regexp above with the code you wish you had
 end
 
   When(/^I press the login button$/) do
-    pending # express the regexp above with the code you wish you had
+    click_on 'Login'
   end
 
   Then(/^I should see the view where I am able to login$/) do
-    pending # express the regexp above with the code you wish you had
+    current_path.should == "/login"
+    page.has_field?('email', :type => 'email_field')
+    page.has_field?('password', :type => 'password_field')
+    page.has_button?('Log in')
   end
+  
+
+
+
+Given(/^the following users have been added to FantasyTrack:$/) do |users_table|
+  # table is a Cucumber::Ast::Table
+  users_table.hashes.each do |user|
+    
+    u = User.new(user)
+    u.user_id = user['user_id']
+    u.email = user['email']
+    u.role = user['role']
+    u.password = user['password']
+    u.save()
+  end
+end  
 
 Given (/^I am on the login screen$/) do
-   pending # express the regexp above with the code you wish you had
+   visit '/login'
 end
-
   When(/^I added an email of "(.*?)" and a password "(.*?)"$/) do |arg1, arg2|
-    pending # express the regexp above with the code you wish you had
+    fill_in('session_email', :with =>  arg1)
+    fill_in('session_password', :with => arg2)
+  end
+   
+  When (/^I press "(.*?)"$/) do |arg1|
+     click_on 'Log in'
+  end
+  
+  Then(/^I should be redirected to homepage$/) do
+    current_path.should == ""
   end
 
-  When(/^I press "(.*?)"$/) do |arg1|
-    pending # express the regexp above with the code you wish you had
+  Then(/^I should see "(.*?)"$/) do |arg1|
+    page.should have_content(arg1)
   end
 
   Then(/^I should be signed in as the administrator$/) do
-    pending # express the regexp above with the code you wish you had
+     expect(page).to have_selector("#admin")
   end
 
   Then(/^I should be signed in as a user who is not the administrator$/) do
-    pending # express the regexp above with the code you wish you had
+    expect(page).not_to have_selector("#admin")
   end
+  
+  
