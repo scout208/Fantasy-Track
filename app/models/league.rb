a3144a4af89b0c45be7ecea5af8442c785e6ddd2
nate_pierotti <nathan-pierotti@uiowa.edu>
2015-11-20 17:46:00 +0000

    almost done

--- app/models/league.rb
+++ app/models/league.rb
@@ -12,7 +12,7 @@ class League < ActiveRecord::Base
 	
  def self.search(search)
    if search
-     find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+     find(:all, :conditions => ['name LIKE ?', "#{search}"])
    else
      find(:all)
    end
