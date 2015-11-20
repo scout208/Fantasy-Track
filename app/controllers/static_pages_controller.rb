eaf246133c9b7b75d5b1b346b233575404c3da4a
scout208 <connor-early@uiowa.edu>
2015-11-20 21:17:09 +0000

    Created Cucumber test for CreateMeets feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,13 +12,13 @@ class StaticPagesController < ApplicationController
       @links = doc.css('section#recentBanners a')
       @titles = doc.css('section#recentBanners').css('strong').to_ary
       @title_images = doc.css('section#recentBanners').css('a img')
-      puts(@titles)
+
       @hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
       @hrefs.to_ary
-      puts(@hrefs)
+
       @srcs = @title_images.map {|images| images.attribute('src').to_s}.delete_if {|src| src.empty?}
       @srcs.to_ary
-      puts(@srcs)
+
 
   end
   
