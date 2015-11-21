2b5801786c7bf46142538f6bf331c367a1bbc66d
yujhuang <yujun-huang@uiowa.edu>
2015-11-21 04:23:00 +0000

    minor error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,21 +12,10 @@ class StaticPagesController < ApplicationController
       @links = doc.css('section#recentBanners a')
       @titles = doc.css('section#recentBanners').css('strong').to_ary
       @title_images = doc.css('section#recentBanners').css('a img')
-<<<<<<< HEAD
       @hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
       @hrefs.to_ary
       @srcs = @title_images.map {|images| images.attribute('src').to_s}.delete_if {|src| src.empty?}
       @srcs.to_ary
-=======
-
-      @hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
-      @hrefs.to_ary
-
-      @srcs = @title_images.map {|images| images.attribute('src').to_s}.delete_if {|src| src.empty?}
-      @srcs.to_ary
-
->>>>>>> create-meet-feature
-
   end
   
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root                'static_pages#home'
+  root            'static_pages#home'
   get    'about'   => 'static_pages#about'
   get   'show_all' => 'leagues#show_all'
   get    'signup'  => 'users#new'
