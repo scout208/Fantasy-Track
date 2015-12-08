a49b6382b4e9fbdfd7eefb1c5ef6c3190ac50b09
scout208 <connor-early@uiowa.edu>
2015-12-08 18:23:14 +0000

    All the tests for this feature run

--- app/models/league_message.rb
+++ app/models/league_message.rb
@@ -3,7 +3,7 @@ class LeagueMessage < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :league_id, presence: true
-  #validates :user_id, presence: true
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
 end
