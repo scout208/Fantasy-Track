e2a7a8a0250daac485b14946e0c9938cb9d9d76e
nate_pierotti <nathan-pierotti@uiowa.edu>
2015-12-09 01:44:02 +0000

    changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,58 +7,15 @@ class StaticPagesController < ApplicationController
     if(logged_in?)
     
     end
-
-      doc = Nokogiri::HTML(open("http://www.flotrack.org/"))
-<<<<<<< HEAD
-      @links = doc.css('div.figure-mask').css('a')
-      @title_images = doc.css('div.figure-mask').css('a img')
-      @titles = @title_images.map{|images| images['alt']}.delete_if {|alt| alt.empty?}
-      @titles.to_ary
-      @hrefs = @links.map {|link| link['href']}.delete_if {|href| href.empty?}
-      @hrefs.to_ary
-      @srcs = @title_images.map {|images| images.attribute('src').to_s}.delete_if {|src| src.empty?}
-      @srcs.to_ary
-=======
-     #@links = doc.css('section#recentBanners a')
-     #@titles = doc.css('section#recentBanners').css('strong').to_ary
-     # @title_images = doc.css('section#recentBanners').css('a img')
-     # @hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
-     # @hrefs.to_ary
-     # @srcs = @title_images.map {|images| images.attribute('src').to_s}.delete_if {|src| src.empty?}
-      #@srcs.to_ary
-      
-      #links = doc.css()
-      
-
-      #@links = doc.css('div.figure-mask').map do |
-      #puts(@links)
-      #puts("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-      #puts("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-      #@titles = doc.css('section#figure-mask').css()
-      #@hrefs = @links.map {|link| link.attribute('href').to_s}.delete_if {|href| href.empty?}
-      #@hrefs.to_ary
   
+      doc = Nokogiri::HTML(open("http://www.flotrack.org/"))
       @links = []
       @titles = []
       headlines = doc.css('div.figure-mask').map do |h|
         @links += [h.at_css("a")['href']]
         @titles += [h.at_css("img").attr('alt')]
-
-        puts(@links)
-        puts(@titles)
-        puts("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
       end
-      
-     # @links.to_ary
-      #@titles.to_ary
-      #puts(@links)
-      #puts(@titles)
-      #puts("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-      
-      
 
-      
->>>>>>> 63dfbf6b67c11cfdcf6f1aab5529d0ac5601703b
   end
   
   def about
